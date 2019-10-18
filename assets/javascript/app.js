043ad49b5a74c3c17b358e595108a68caaad9956
Huan Nguyen <huancal1984@gmail.com>
2019-10-18 13:12:50 -0700

    js raw logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,82 @@
 $(document).ready(function () {
 
     $('#startbutton').on('click', function () {
-        var number = 30;
-        setInterval(function () {
-            number--;
-            if (number >= 0) {
-                span = document.getElementById("show-number");
-                span.innerHTML = number + ' Seconds ';
-            }
-            if (number === 0) {
-                alert('sorry, out of time');
-                clearInterval(number);
-            }
-        }, 1000);
+        TriviaGame.start()
+        // var number = 60;
+        // setInterval(function () {
+        //     number--;
+        //     if (number >= 0) {
+        //         span = document.getElementById("show-number");
+        //         span.innerHTML = number + ' Seconds ';
+        //     }
+        //     if (number === 0) {
+        //         alert('sorry, out of time');
+        //         clearInterval(number);
+        //     }
+        // }, 1000);
 
     })
+
+    // Question set
+    var triviaQuestions = [{
+        question: "What was the first full length CGI movie?",
+        answers: ["A Bug's Life", "Monsters Inc.", "Toy Story", "The Lion King"],
+        correctAnswer: "Toy Story",
+        image: "assets/images/toystory.gif"
+    }, {
+        question: "Which of these is NOT a name of one of the Spice Girls?",
+        answers: ["Sporty Spice", "Fred Spice", "Scary Spice", "Posh Spice"],
+        correctAnswer: "Fred Spice",
+        image: "assets/images/spicegirls.gif"
+    }]
+
+    var TriviaGame = {
+        correct_answers: 0,
+        incorrect_answers: 0,
+        unansweredd_questions: 0,
+        timer: 60,
+
+        start: function () {
+            var number = 60;
+            setInterval(function () {
+                number--;
+                if (number >= 0) {
+                    span = document.getElementById("show-number");
+                    span.innerHTML = number + ' Seconds ';
+                }
+                if (number === 0) {
+                    alert('sorry, out of time');
+                    clearInterval(number);
+                }
+            }, 1000);
+
+            for (var i = 0; i < triviaQuestions.length; i++) {
+                $('#Question1').append("<h1> " + triviaQuestions[i].question + "</h1>")
+
+                for (var j = 0; j < triviaQuestions[i].answers.length; j++) {
+                    $('#Question1').append("<div class ='radio'><input type='radio' name='option" + j +
+                        "' value='" + triviaQuestions[i].answers[j] + "''> " + triviaQuestions[i].answers[j] + "</div>")
+                }
+            }
+        },
+
+        userchoice: function () {
+            for (var i = 0; i < triviaQuestions.length; i++) {
+                var radioValue = $("input[name='option-" + i + "']:checked").val();
+                if (radioValue === triviaQuestions[i].correctAnswer) {
+                    console.log('correct answer');
+                    TriviaGame.correct_answers++;
+
+                } else if (radioValue === undefined) {
+                    console.log("no guess");
+                    TriviaGame.unansweredd_questions++;
+                } else {
+                    console.log('wrong answer');
+                    TriviaGame.incorrect_answers++;
+                }
+            }
+        }
+    }
 
 
 
