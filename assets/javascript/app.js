12727dc56faa2158e6ffc27ff2616afeaef6dbb6
Huan Nguyen <huancal1984@gmail.com>
2019-10-18 03:45:45 -0700

    timer added for js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,22 @@
 $(document).ready(function () {
 
     $('#startbutton').on('click', function () {
-        $('body').append('hi')
+        var number = 30;
+        setInterval(function () {
+            number--;
+            if (number >= 0) {
+                span = document.getElementById("show-number");
+                span.innerHTML = number + ' Seconds ';
+            }
+            if (number === 0) {
+                alert('sorry, out of time');
+                clearInterval(number);
+            }
+        }, 1000);
 
-    });
+    })
 
-})
+
+
+
+});
